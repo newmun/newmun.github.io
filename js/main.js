a8c90b81477cfff283ca09f6b5644594c53e8557
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-13 18:36:13 +0300

    Reduce delay time for preloader

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   // Preloader (if the #preloader div exists)
   $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
+      $('#preloader').delay(5).fadeOut('slow', function () {
         $(this).remove();
       });
     }
